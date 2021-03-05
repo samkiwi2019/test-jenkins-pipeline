03eaf5f83202ffc75810d677dcdfae1c2a3f4ab5
Sam <77814420+samkiwi2019@users.noreply.github.com>
2021-03-05 14:53:33 +1300

    updated function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 console.log('I am coming!');
 
 let n = 5;
-for(let i =0; i<n;  i++){
-  for(let j=0; j<i; j++){
-    console.log("*") 
-  }
+let str='';
+for(let i=1;i<=10;i++){
+   for(let j=i; j<=10;j++){
+       let str=str +'⭐';
+   }
+   str=str + '\n';
 }
+console.log(str);
 
 console.log('finished');
